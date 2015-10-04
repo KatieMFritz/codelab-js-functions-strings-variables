67a83b3aa03545f290c4d0c5452dcebc2089d7b0
Katie Fritz <katie@katiemfritz.com>
2015-10-03 22:16:52 -0400

    project meets requirements!

--- scripts.js
+++ scripts.js
@@ -1,5 +1,15 @@
+window.onload = function () {
+
 var name = prompt("What's your name?")
 
 var color = prompt("What's your favorite color?")
 
 var catname = prompt("What's your cat's name?")
+
+document.getElementById('name').innerHTML = name;
+
+document.getElementById('color').innerHTML = color;
+
+document.getElementById('catname').innerHTML = catname;
+
+}
