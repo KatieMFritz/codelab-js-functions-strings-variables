138bbf78b22f95e8ded66b47939ee777970c1724
Katie Fritz <katie@katiemfritz.com>
2015-10-04 20:55:44 -0400

    fix indentation issues

--- scripts.js
+++ scripts.js
@@ -1,15 +1,11 @@
 window.onload = function () {
 
-var name = prompt("What's your name?")
+  var name = prompt("What's your name?")
+  var color = prompt("What's your favorite color?")
+  var catname = prompt("What's your cat's name?")
 
-var color = prompt("What's your favorite color?")
-
-var catname = prompt("What's your cat's name?")
-
-document.getElementById('name').innerHTML = name;
-
-document.getElementById('color').innerHTML = color;
-
-document.getElementById('catname').innerHTML = catname;
+  document.getElementById('name').innerHTML = name;
+  document.getElementById('color').innerHTML = color;
+  document.getElementById('catname').innerHTML = catname;
 
 }
